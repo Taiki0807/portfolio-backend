09f91c8296ec913b883649a1394d6b41cf167fcd
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-20 01:46:12 +0900

    [add] Add serializer method to return only table of contents (TOC) of the instance

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Category, Post
 import markdown
+from markdown.extensions.toc import TocExtension
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -21,10 +22,18 @@ class SimplePostSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     main_text = serializers.SerializerMethodField()
+    toc_text = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
-    
+
     def get_main_text(self, instance):
-        return markdown.markdown(instance.main_text, extensions=['toc'])
+        main_text = instance.main_text.replace("[TOC]", "")  # [TOC] を削除
+        return markdown.markdown(main_text)
+    
+    def get_toc_text(self, instance):
+        toc_html = markdown.markdown(instance.main_text, extensions=[TocExtension(toc_depth=1)])
+        toc_start_index = toc_html.find('<ul>')
+        toc_end_index = toc_html.rfind('</ul>') + len('</ul>')
+        return toc_html[toc_start_index:toc_end_index]
