706a68b761582ffe8afcb0d48fc86a36f19aa98c
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-25 14:40:13 +0900

    [add]  add delay middleware

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog.middleware.DelayMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
