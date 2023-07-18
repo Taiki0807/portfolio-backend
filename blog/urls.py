c9d450ba35414907171637a69484b9a56f296bf7
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-19 00:11:27 +0900

    [add] Add upload image api

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('categories/', views.CategoryList.as_view(), name='category_list'),
     path('tags/', views.TagList.as_view(), name='tag_list'),
+    path('upload/', views.ImageRegisterAPIView.as_view(), name="imageRegister"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,status
 from .models import Post, Category,Tag
 from .serializers import CategorySerializer, PostSerializer, SimplePostSerializer,TagSerializer
+from django.core.files.storage import FileSystemStorage
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from google.cloud import storage
+import random,string
+import os
+from datetime import datetime
+from django.utils import timezone
+from google.oauth2 import service_account
 
+#google cloud storageのクライアントインスタンスを作成
+key_path = './portfolio-377405-3a43641d221e.json'
+credential = service_account.Credentials.from_service_account_file(key_path)
+
+project_id = "portfolio-377405"
+client = storage.Client(project_id, credentials=credential)
+
+#バケットのインスタンスを取得
+bucket = client.bucket('portfolio-hosokawalab')
 
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
@@ -20,3 +39,25 @@ class PostList(generics.ListAPIView):
 class PostDetail(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+def upload(file):
+    dt_now = datetime.now()
+    dt_now_str = dt_now.strftime('%Y%m%d%H%M%S%f')
+    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(10)]
+    file_name_with_extention = dt_now_str + "_" + ''.join(randlst) + '.jpg'
+    fs = FileSystemStorage()
+    filename = fs.save("images/" + file_name_with_extention, file)
+    blob = bucket.blob(file_name_with_extention)
+    blob.upload_from_filename(fs.path(filename))
+    os.remove(fs.path(filename))
+
+    return blob.public_url
+
+
+class ImageRegisterAPIView(APIView):
+    parser_classes = [FormParser, MultiPartParser]
+
+    def post(self, request, *args, **kwargs):
+        file = request.data["image"]
+        object_url = upload(file)
+        return Response({"image_code": f"![]({object_url})"}, status.HTTP_201_CREATED)
