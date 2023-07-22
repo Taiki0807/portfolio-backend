f92de3a0b56a3c604cc854d355c6b38d9004f09d
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-22 16:38:32 +0900

    [fix] Order Works by creation date

--- works/views.py
+++ works/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Works,Category,Tag
 from .serializers import WorksSerializer,SimpleWorksSerializer,CategorySerializer,TagSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
@@ -11,8 +13,12 @@ class TagList(generics.ListAPIView):
     serializer_class = TagSerializer
 
 class WorksListCreateView(generics.ListCreateAPIView):
-    queryset = Works.objects.all()
+    queryset = Works.objects.order_by('-created_at')  # '-' indicates descending order
     serializer_class = SimpleWorksSerializer
+
+    @method_decorator(cache_page(60*15))  # Cache the response for 15 minutes (optional)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class WorksRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Works.objects.all()
